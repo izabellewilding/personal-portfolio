f8c4ccd57019a9de564fec07459cb443f1ffbb62
izabelle wilding <izabellewilding@gmail.com>
2020-03-09 10:28:50 +0000

    Diabled card flp for mobile and added buttons

--- src/components/card-front.js
+++ src/components/card-front.js
@@ -3,13 +3,14 @@ import Img from "../components/image"
 import "../styles/card-front.scss"
 import "../styles/projects.scss"
 import styled from "styled-components"
+import GitHub from "../assets/github-darkRed.svg"
 
 const Gradient = styled.div`
   background: linear-gradient(
     0deg,
     rgba(0, 0, 0, 1) 7%,
-    rgba(0, 9, 36, 0.66) 40%,
-    rgba(0, 0, 0, 0.34) 64%
+    rgba(0, 9, 36, 0.82) 40%,
+    rgba(0, 8, 9, 0.62) 64%
   );
   height: 100%;
   width: 100%;
@@ -32,9 +33,22 @@ const CardFront = ({ ...props }) => {
         <Img src={props.cardImage} className="card-img " />
       </div>{" "}
       <Gradient className=" absolute top-0 bottom-0 right-0 left-0" />
-      <p className="absolute bottom-0 text-white text-2xl uppercase flex font-sans p-5">
+      <p className="absolute text-white text-2xl uppercase flex font-sans p-5 md:bottom-0">
         {props.cardTitle}
       </p>
+      <div className="card-back-buttons bottom-0 absolute visible md:invisible md:text-center">
+        <a href={props.siteLink} target="_blank" rel="noopener noreferrer">
+          <button className="visit-site-button">View Site</button>
+        </a>{" "}
+        {/* <CardWave className="card-wave" /> */}
+        <a href={props.gitLink} target="_blank" rel="noopener noreferrer">
+          <button className="visit-site-button">
+            {" "}
+            <GitHub className="git-button-icon" />
+            Git
+          </button>
+        </a>
+      </div>
     </div>
   )
 }
