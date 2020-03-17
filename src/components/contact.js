2af6a00943d358b72b9947e921e23bbbd9fa3ca4
izabelle wilding <izabellewilding@gmail.com>
2020-03-17 20:54:39 +0000

    Linkedin svg tweak

--- src/components/contact.js
+++ src/components/contact.js
@@ -45,7 +45,7 @@ const Contact = React.forwardRef((props, ref) => {
                 href="https://github.com/izabellewilding"
                 target="_blank"
                 rel="noopener noreferrer"
-                className=" bg-white  flex shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className=" bg-white flex shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <GitHub className="social-icon-footer" />{" "}
@@ -54,7 +54,7 @@ const Contact = React.forwardRef((props, ref) => {
                 href="https://www.linkedin.com/in/izabelle-wilding-b68a73130/"
                 target="_blank"
                 rel="noopener noreferrer"
-                className=" bg-white  shadow-lg flex font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
+                className=" bg-white shadow-lg flex font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
                 <LinkedIn className="social-icon-footer" />
