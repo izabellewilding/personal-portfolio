4353a6af41f0d13ed2e1bc74f5b2ed12c7f5486e
izabelle wilding <izabellewilding@gmail.com>
2020-07-23 17:00:09 +0100

    Removed unused images

--- gatsby-config.js
+++ gatsby-config.js
@@ -127,18 +127,18 @@ module.exports = {
       },
     },
 
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        // icon: `src/images/text-logo2.png`,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-manifest`,
+    //   options: {
+    //     name: `Iza`,
+    //     short_name: `starter`,
+    //     start_url: `/`,
+    //     background_color: `#663399`,
+    //     theme_color: `#663399`,
+    //     display: `minimal-ui`,
+    //     // icon: `src/images/text-logo2.png`,
+    //   },
+    // },
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
