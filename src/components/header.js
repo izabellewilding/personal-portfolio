30e8e6299196e86c0a73dd74182350cf74437c6c
izabelle wilding <izabellewilding@gmail.com>
2020-07-23 12:08:07 +0100

    Changed drawer back to css

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,6 @@ import React, { useState } from "react"
 import { Link } from "gatsby"
 import Logo from "../assets/text-logo2.svg"
 import GitHub from "../assets/github-darkRed.svg"
-import Email from "../assets/email-darkRed.svg"
 import LinkedIn from "../assets/linkedin-darkRed.svg"
 import Twitter from "../assets/twitter.svg"
 import Medium from "../assets/medium.svg"
@@ -11,8 +10,6 @@ import classNames from "classnames"
 import { Drawer, DrawerHeader, DrawerTitle, DrawerContent } from "@rmwc/drawer"
 import styled from "styled-components"
 import { List } from "@rmwc/list"
-import "@material/list/dist/mdc.list.min.css"
-import "@material/drawer/dist/mdc.drawer.min.css"
 
 const StyledDrawer = styled(Drawer)`
   background-color: #ffffff;
